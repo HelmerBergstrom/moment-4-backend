4c65ddc400140082216fd8e097ecb1f5fe6a6144
Helmer Bergström <hebe2400@student.miun.se>
2025-05-21 20:43:14 +0200

    Lägger till routes för register och login. Skapar schema och connection till MongoDB. Gör if-satser för att kontrollera inmatning i registrerings-routen.

--- routes/theRoutes.js
+++ routes/theRoutes.js
@@ -3,12 +3,44 @@
 const express = require("express");
 const router = express.Router();
 
-router.post("/login", async (res, req) => {
-    console.log("Login called...");
+router.post("/register", async (req, res) => {
+    try {
+        const { username, password, email } = req.body;
+
+        let array = [];
+
+        // Användarnamn måsta vara minst 7 tecken långt.
+        if(!username || username.length < 7) {
+            array.push("Vänligen skapa ett användarnamn!")
+            return res.status(400).json({ error: "Vänligen fyll i användarnamn!" });
+        }
+
+        if(!password) {
+            array.push("Vänligen skapa ett lösenord!")
+            return res.status(400).json({ error: "Vänligen fyll i lösenord!" });
+        }
+        // Epost måste inkludera @ och en punkt.
+        if(!email || !email.includes("@" && ".")) {
+            array.push("Vänligen ange epost!")
+            return res.status(400).json({ error: "Vänligen fyll i epost!" });
+        }
+
+        // Om det har fyllt i korrekt = skapa användare.
+        res.status(201).json({ message: "Användare skapad!"});
+    } catch(error) {
+        res.status(500).json({ error: "Server error!"});
+    }
 });
 
-router.post("/register", async (res, req) => {
-    console.log("Register called...");
+
+router.post("/login", async (req, res) => {
+    try {
+
+        res.status(201).json({ message: "Logged in! "});
+    } catch(error) {
+        res.status(500).json({ error: "Server error!" });
+    }
 });
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,16 +7,34 @@ const theRoutes = require("./routes/theRoutes");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3002;
+const port = process.env.PORT || 3001;
 app.use(bodyParser.json());
 
 app.use("/api", theRoutes);
 
-mongoose.connect("mongodb://127.0.0.1:27017/api").then(() => {
+mongoose.connect("mongodb://127.0.0.1:27017/user-pass").then(() => {
     console.log("Connected to MongoDB :)")
 }).catch((error) => {
     console.log("Error connecting to database: " + error); 
-}) 
+});
+
+// const UserSchema = new mongoose.Schema({
+//     username: {
+//         type: String,
+//         required: true
+//     },
+//     email: {
+//         type: String,
+//         required: true
+//     },
+//     password: {
+//         type: String,
+//         required: true
+//     },
+//     dateCreated: {
+//         type: Date,
+//     }
+// }); 
 
 app.listen(port, () => {
     console.log("Server running on http://localhost:" + port)
