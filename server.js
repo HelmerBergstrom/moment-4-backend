aec98eff5624273e609b63227e73bc60133c6130
Helmer Bergström <hebe2400@student.miun.se>
2025-06-12 12:42:44 +0200

    Hämtar användare via app.get och kör funktionen för att kontrollera token i denna. Hämtar användarnamn men inte lösenord!

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const theRoutes = require("./routes/theRoutes");
 const cors = require("cors");
 require("dotenv").config();
 
+const User = require("./models/User");
+
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(bodyParser.json());
@@ -21,20 +23,29 @@ app.get("/api/secret", authenticateToken, (req, res) => {
     res.json({ message: "Skyddad route..." });
 });
 
+app.get("/api/users", authenticateToken, async (req, res) => {
+    try {
+        const users = await User.find({}, "-password"); // Exkludera lösenord
+        res.status(200).json(users);
+    } catch (err) {
+        return res.status(500).json({ message: "Något gick fel vid hämtning av användare" });
+    }
+});
+
 function authenticateToken(req, res, next) {
     const headerAuth = req.headers['authorization'];
     const token = headerAuth && headerAuth.split(' ')[1];
 
-    if(token === null) {
-        res.status(401).json({ message: "Token missing!" })
+    if(!token) {
+        return res.status(401).json({ message: "Token saknas! Du är inte behörig.." })
     }
 
-    jwt.verify(token, process.env.JWT_KEY, (err, username) => {
+    jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
         if(err) { 
-            return res.status(403).json({ message: "Felaktig Token!" })
+            return res.status(403).json({ message: "Åtkomst nekad! Token ogiltlig eller utgången." })
         }
 
-        req.username = username;
+        req.username = decoded.username;
         next();
     })
 }
